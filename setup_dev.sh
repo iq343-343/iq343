13601c819379793b1694a09950bf99529b531d8f
iq343-343 <burdin.md@gmail.com>
2026-02-24 11:04:52 +0500

    Fix Nginx double escaping in setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -95,40 +95,40 @@ server {
     index index.html;
 
     location / { 
-        try_files \$uri \$uri/ /index.html; 
+        try_files \\\$uri \\\$uri/ /index.html; 
     }
 
     location /extragram/ {
         alias /var/www/extract-studio-dev/extragram/dist/;
-        try_files \$uri \$uri/ /extragram/index.html;
+        try_files \\\$uri \\\$uri/ /extragram/index.html;
     }
 
     location ^~ /extrapars/api/ {
         proxy_pass http://localhost:3002/api/;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Upgrade \\\$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \\\$host;
+        proxy_cache_bypass \\\$http_upgrade;
+        proxy_set_header X-Real-IP \\\$remote_addr;
+        proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
     }
 
     location ^~ /extrapars/ {
         alias /var/www/extract-studio-dev/extrapars/public/;
         index index.html;
-        try_files \$uri \$uri/ /extrapars/index.html;
+        try_files \\\$uri \\\$uri/ /extrapars/index.html;
     }
 
     location /api/ {
         proxy_pass http://localhost:3001;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Upgrade \\\$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \\\$host;
+        proxy_cache_bypass \\\$http_upgrade;
+        proxy_set_header X-Real-IP \\\$remote_addr;
+        proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
     }
 }
 EOF
